c85d24c3f50106d95457d8b4b8bdae6aedbdaf86
Konrad Macieja <konrad.macieja@gmail.com>
2019-04-09 20:44:10 +0200

    updated login/logout settings

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# LOGIN_URL = 'login'
-# LOGIN_REDIRECT_URL = 'index'
-# LOGOUT_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
